b74b6860a501386a357e254b14d31e37f446fcee
overcch <overcch@gmail.com>
2026-04-14 12:26:18 +0800

    fix style

--- apps/web/src/components/FeaturedEquipment.tsx
+++ apps/web/src/components/FeaturedEquipment.tsx
@@ -32,7 +32,7 @@ export function FeaturedEquipment({ section, locale }: CmpProps) {
           <div className="flex flex-row w-full">
             {
               ((section.payload?.dynamic?.[0] as any)?.equipment as Equipment[])?.map((xs, idx) => (
-                <Link target="_blank" href={`/${locale}/resources/equipment/${xs.documentId}`} key={xs.id} className="w-[78.205128vw] ml-5 shrink-0 text-left xl:w-76.25 xl:ml-0 xl:mr-5 hover:drop-shadow-[0_8px_24px_rgba(0,0,0,0.08)] bg-[#FAFAFA]">
+                <Link target="_blank" href={`/${locale}/resources/equipment/${xs.documentId}`} key={xs.id} className="w-[78.205128vw] ml-5 shrink-0 text-left xl:w-76.25 xl:ml-0 xl:mr-5 hover:drop-shadow-[0_8px_24px_rgba(0,0,0,0.08)] bg-[#FAFAFA] transition-colors duration-300">
 
                   <div className="relative w-76.25 h-55 shrink-0 overflow-hidden flex items-center justify-center">
                     <Image fill className="object-cover -z-10" src={Assets.EquipBase} alt="" />
--- apps/web/src/components/FeaturedMaterial.tsx
+++ apps/web/src/components/FeaturedMaterial.tsx
@@ -43,7 +43,7 @@ export function FeaturedMaterial({ section, siteData, locale }: CmpProps) {
           <div className="flex flex-row">
             {
               ((section.payload?.dynamic?.[0] as any)?.materials as Material[])?.map((xs, idx) => (
-                <Link target="_blank" href={`/${locale}/resources/material/${xs.documentId}`} key={idx} className="shrink-0 w-[78.205128vw] bg-[#fafafa] rounded-lg overflow-hidden mr-5 xl:w-76.25 hover:drop-shadow-[0_8px_24px_rgba(0,0,0,0.08)]">
+                <Link target="_blank" href={`/${locale}/resources/material/${xs.documentId}`} key={idx} className="shrink-0 w-[78.205128vw] bg-[#fafafa] rounded-lg overflow-hidden mr-5 xl:w-76.25 hover:drop-shadow-[0_8px_24px_rgba(0,0,0,0.08)] transition-colors duration-300">
                   <Image className="object-cover w-full rounded-lg xl:h-40" width={305} height={160} src={getStrapiMedia(xs.icon) ?? ""} alt="" />
                   <div className="px-5 text-left">
                     <p className="my-5 text-lg font-semibold leading-none">{xs.name}</p>
